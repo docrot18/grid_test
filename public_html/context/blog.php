f17149a67e3069b1a002994e0708c257f831bef0
docrot18 <80437818+docrot18@users.noreply.github.com>
2023-03-06 17:44:33 +0900

    fixes

--- public_html/context/blog.php
+++ public_html/context/blog.php
@@ -2,57 +2,49 @@
     'blog' => [
         [
             'class' => 'blog-photo-item',
-            'subclass' => 'blog-photo-item1',
+            'subclass' => 'blog-photo-2x1',
             'img' => 'images/1.jpg',
         ],
         [
             'class' => 'blog-photo-item',
-            'subclass' => 'blog-photo-item2',
             'img' => 'images/2.jpg',
         ],
         [
             'class' => 'blog-photo-item',
-            'subclass' => 'blog-photo-item3',
             'img' => 'images/3.png',
         ],
         [
             'class' => 'blog-photo-item',
-            'subclass' => 'blog-photo-item4',
             'img' => 'images/4.png',
         ],
         [
             'class' => 'blog-photo-item',
-            'subclass' => 'blog-photo-item5',
             'img' => 'images/5.png',
         ],
         [
             'class' => 'blog-photo-item',
-            'subclass' => 'blog-photo-item6',
+            'subclass' => 'blog-photo-2x1',
             'img' => 'images/6.png',
         ],
         [
             'class' => 'blog-photo-item',
-            'subclass' => 'blog-photo-item7',
+            'subclass' => 'blog-photo-2x2',
             'img' => 'images/7.png',
         ],
         [
             'class' => 'blog-photo-item',
-            'subclass' => 'blog-photo-item8',
             'img' => 'images/8.png',
         ],
         [
             'class' => 'blog-photo-item',
-            'subclass' => 'blog-photo-item9',
             'img' => 'images/9.png',
         ],
         [
             'class' => 'blog-photo-item',
-            'subclass' => 'blog-photo-item10',
             'img' => 'images/10.png',
         ],
         [
             'class' => 'blog-photo-item',
-            'subclass' => 'blog-photo-item11',
             'img' => 'images/11.png',
         ],
     ],
